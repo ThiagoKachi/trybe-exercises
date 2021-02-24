f150531116d0f406ae89972d1768008694ac883b
Thiago Kachinsky <tjk091@gmail.com>
2021-02-24 18:56:21 -0300

    Exercícios dia 1 - ex01 ao ex05

---
+++ Fundamentos-do-Des-Web/introducao-javascript-logica-programacao/dia_1/ex03/script.js
@@ -0,0 +1,11 @@
+let num1 = 10
+let num2 = 2
+let num3 = 5
+
+if (num1 > num2 && num1 > num3) {
+    console.log(`O número ${num1} é o maior`)
+} else if (num2 > num1 && num2 > num3) {
+    console.log(`O número ${num2} é o maior`)
+} else {
+    console.log(`O número ${num3} é o maior`)
+}
---
+++ Fundamentos-do-Des-Web/introducao-javascript-logica-programacao/dia_1/ex04/script.js
@@ -0,0 +1,9 @@
+let checkNumber = -1
+
+if (checkNumber >= 0) {
+    console.log('Positive')
+} else if (checkNumber < 0) {
+    console.log('Negative')
+} else {
+    console.log('Valor inválido')
+}
