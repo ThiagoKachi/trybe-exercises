68043c1702cacbd3179f75fbdc858113897f09f7
Thiago Kachinsky <tjk091@gmail.com>
2021-03-23 19:34:04 -0300

    Exercícios ES6

---
+++ Fundamentos-do-Des-Web/introducao-a-javascript-es6-e-testes-unitarios/dia_1/script.js
@@ -0,0 +1,23 @@
+const testingScope = (escopo) => {
+  if (escopo === true) {
+    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+  console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+}
+
+testingScope(true);
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+oddsAndEvens.sort((a, b) => {
+  if (a > b) return 1;
+  if (a < b) return -1;
+  return 0;
+})
+
+console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
